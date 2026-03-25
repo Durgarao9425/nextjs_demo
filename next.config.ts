b0cc49e02d1307680004bed8f47d4da6cbf5cf07
Veer Durgarao <veerdurgarao.gotiparthi@rnitss.com>
2026-03-26 00:05:27 +0530

    Final fix: all images supported with unoptimized mode and global HTTPS permissions

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'fakestoreapi.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
+        hostname: '**',
       },
     ],
   },
